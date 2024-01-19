3f19f90d89f9c710c5e940291fb557f1e9ccc040
faroukelhamdi <farouk.elhamdi@gmail.com>
2024-01-19 20:07:59 +0200

    updated icons for folders android and ios

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text(
+              'I Am Rich',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+            centerTitle: true,
+          ),
+          body: const Center(
+            child: Center(
+              child: Image(
+                image: AssetImage('assets/images/diamond.png'),
+              ),
+            ),
+          )),
+    ),
+  );
+}
